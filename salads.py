8e13781cfeb6a96ac0c6eaf4cba706e37d1bc749
mastamysta <benjamin-read@hotmail.co.uk>
2021-04-06 12:00:28 +0100

    Added salad script to print piped data

---
+++ salads.py
@@ -0,0 +1,11 @@
+import sys
+
+def salads(fruits):
+    salads = [split, sandwich, delight, pie]
+    for fruit in fruits:
+        for salad in salads:
+            print(fruit + salad)
+
+if __name__ == "__main__":
+    fruits = sys.argv[1]
+    print(salads(fruits))
