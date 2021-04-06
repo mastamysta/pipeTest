6bd2e346e36183b78f761e102808024e9f0ff5f4
mastamysta <benjamin-read@hotmail.co.uk>
2021-04-06 12:36:32 +0100

    Swapped away from printing fruits list to terminal

--- fruits.py
+++ fruits.py
@@ -2,10 +2,10 @@ def fruits():
     fruitList=["apple", "pear", "banana"]
     return(fruitList)
 
-if __name__ == "__main__":
-    f = fruits()
-    for p in f:
-        print(p)
+# if __name__ == "__main__":
+#     f = fruits()
+#     for p in f:
+#         print(p)
 
-# def main():
-#     return(fruits())
+def main():
+    return(fruits())
